df4e3eb9872fef45b83cd2abda2aa9d69b30fb9b
Mircea MATEI <matei_tm@yahoo.com>
2019-03-23 01:42:09 +0200

    #6. Adding Travis CI helpers

---
+++ m8m/travis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# travis.sh script for CI integration
+
+SDK_URL="https://developer.garmin.com/downloads/connect-iq/sdks/connectiq-sdk-win-2.4.2.zip"
+SDK_FILE="sdk.zip"
+SDK_DIR="sdk"
+
+PEM_FILE="/tmp/developer_key.pem"
+DER_FILE="/tmp/developer_key.der"
+
+###
+
+wget -O "${SDK_FILE}" "${SDK_URL}"
+unzip "${SDK_FILE}" "bin/*" -d "${SDK_DIR}"
+
+openssl genrsa -out "${PEM_FILE}" 4096
+openssl pkcs8 -topk8 -inform PEM -outform DER -in "${PEM_FILE}" -out "${DER_FILE}" -nocrypt
+
+export MB_HOME="${SDK_DIR}"
+export MB_PRIVATE_KEY="${DER_FILE}"
+
+./mb_runner/mb_runner.sh package
