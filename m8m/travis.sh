5c56fbb5058af039be7bc5b65df7b5fcf66c5313
Mircea MATEI <matei_tm@yahoo.com>
2021-07-04 03:56:11 +0300

    #9. Moving sdk back to 2.4.2

--- m8m/travis.sh
+++ m8m/travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # travis.sh script for CI integration
 
-SDK_URL="https://developer.garmin.com/downloads/connect-iq/sdks/connectiq-sdk-lin-4.0.3-2021-06-01-b7cc78017.zip"
+SDK_URL="https://developer.garmin.com/downloads/connect-iq/sdks/connectiq-sdk-lin-2.4.2.zip"
 SDK_FILE="sdk.zip"
 SDK_DIR="sdk"
 
